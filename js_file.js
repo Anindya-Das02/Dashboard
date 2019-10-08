498551383067ba080b34f4ff36d084a39a2f4698
Anindya Das <anindyadas_indra@srmuniv.edu.in>
2019-10-09 02:02:11 +0530

    Update js_file.js

--- js_file.js
+++ js_file.js
@@ -1,3 +1,27 @@
-// window.onload = function(){
-// 	alert("hello");
-// }
+window.onload = function(){
+	var months = ['feburary','march','june','july','august','september','october','november','december'];
+	
+	var e = document.getElementById("select_id");
+	for(let i=0;i<months.length;i++){
+		var opt = document.createElement('option');
+		opt.innerHTML = months[i];
+		e.appendChild(opt);
+	}
+
+}
+
+function large(ele_id){
+	var e = document.getElementById(ele_id);
+	e.style.height = '130px';
+	e.style.border= '2px solid orange';
+	e.style.backgroundColor = "#1d1138";
+
+}
+
+function small(ele_id){
+	var e = document.getElementById(ele_id);
+	e.style.height = '102px';
+	e.style.border= '2px solid white';
+	e.style.backgroundColor = "#110824";
+}
+
